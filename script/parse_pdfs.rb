1fadb0eee76752485c4a83f0e72d854e796b1b32
MJ Rossetti <datacreativellc@gmail.com>
2017-03-31 22:08:00 -0400

    Save pdf file locally and it looks fine

--- script/parse_pdfs.rb
+++ script/parse_pdfs.rb
@@ -9,7 +9,7 @@ school_name = "YALE UNIVERSITY"
 local_file = File.join(File.expand_path("../../tmp/reports", __FILE__), "my_report.pdf")
 
 agent = Mechanize.new
-agent.pluggable_parser.pdf = Mechanize::Download # Mechanize::FileSaver
+agent.pluggable_parser.pdf = Mechanize::File # Mechanize::FileSaver Mechanize::Download
 page = agent.get(URL)
 
 year_selector = page.form.field_with(:name => 'ddlYear')
@@ -23,18 +23,19 @@ school_option.select
 form = page.forms.first
 button = page.form.button_with(:value => "Generate Report")
 response = agent.submit(form, button)
-puts response.filename
-puts response.body_io
-
-io = response.body_io
-reader = PDF::Reader.new(io) #> PDF::Reader::MalformedPDFError: PDF does not contain EOF marker
 
 binding.pry
 
 
+response.save_as(local_file)
+
+#io = response.body_io
+#reader = PDF::Reader.new(io) #> PDF::Reader::MalformedPDFError: PDF does not contain EOF marker
 
 
-#remote_file = response.body
+
+
+
 
 #FileUtils.rm_rf(local_file)
 
