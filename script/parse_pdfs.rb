75dfe5b4a6e0a597cbb94ccab29e0c9c77cc0d2c
MJ Rossetti <datacreativellc@gmail.com>
2017-04-28 21:59:39 -0400

    Download PDF from source; observe it is malformatted

--- script/parse_pdfs.rb
+++ script/parse_pdfs.rb
@@ -4,41 +4,61 @@ require "mechanize"
 require "pdf-reader"
 
 URL = "http://employmentsummary.abaquestionnaire.org/"
-year = 2015
-school_name = "YALE UNIVERSITY"
-local_file = File.join(File.expand_path("../../tmp/reports", __FILE__), "my_report.pdf")
 
 agent = Mechanize.new
-agent.pluggable_parser.pdf = Mechanize::File # Mechanize::FileSaver Mechanize::Download
+# agent.pluggable_parser.pdf = Mechanize::Download # Mechanize::File # Mechanize::FileSaver Mechanize::Download
 page = agent.get(URL)
 
+#
+# SELECT YEAR
+#
+
+year = 2015
 year_selector = page.form.field_with(:name => 'ddlYear')
 year_option = year_selector.options.find{|option| option.text == year.to_s}
 year_option.select
 
+#
+# SELECT SCHOOL
+#
+
+school_name = ["YALE UNIVERSITY", "CALIFORNIA-BERKELEY, UNIVERSITY OF"].sample
 school_selector = page.form.field_with(:name => 'ddlUniversity')
 school_option = school_selector.options.find{|option| option.text == school_name}
 school_option.select
 
+#
+# SUBMIT FORM
+#
+
 form = page.forms.first
 button = page.form.button_with(:value => "Generate Report")
-response = agent.submit(form, button)
+response = agent.submit(form, button) #> Mechanize::File
+
+#
+# DOWNLOAD PDF FILE
+#
+
+local_file = File.join(File.expand_path("../../tmp", __FILE__), "my_report.pdf")
+
+FileUtils.rm_rf(local_file)
+
+response.save(local_file)
+
+#
+# PARSE PDF FILE
+#
+
+io = local_file
+
+reader = PDF::Reader.new(io) #> PDF::Reader::MalformedPDFError: PDF does not contain EOF marker
 
 binding.pry
 
 
-response.save_as(local_file)
-
-#io = response.body_io
-#reader = PDF::Reader.new(io) #> PDF::Reader::MalformedPDFError: PDF does not contain EOF marker
-
-
-
-
-
+#puts "READING PDF WITH #{reader.pages.count} PAGES"
 
 #FileUtils.rm_rf(local_file)
-
 
 # text = IO.read(remote_file, mode: 'rb') # > ArgumentError: string contains null byte
 # File.open(local_file, 'wb'){|f| f << remote_file}
@@ -50,8 +70,6 @@ response.save_as(local_file)
 #    local_file.write(remote_file.read)
 #  end
 #end # > ArgumentError: string contains null byte
-
-
 
 #io = response.body
 #reader = PDF::Reader.new(io)
